ca7828437eb769a30ff27c0cbaa467533b96a83b
xayyeem <kjafri30izback@gmail.com>
2024-12-21 00:57:58 +0530

    setting up sequelize

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { PORT } = require('../config/serverConfig')
+const { PORT } = require('./config/serverConfig')
 
 const setUpAndStartServer = async () => {
+    
     const app = express();
+
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     })
